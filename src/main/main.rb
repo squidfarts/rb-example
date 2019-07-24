fe124b10f232c1ff8481510a96122f4740315ef2
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 18:20:47 -0700

    Add files via upload

---
+++ src/main/main.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+###################################################################################
+#                                                                                 #
+# NAME: main.rb                                                                   #
+#                                                                                 #
+# AUTHOR: Michael Brockus.                                                        #
+#                                                                                 #
+# CONTACT: <mailto:michael@squidfarts.com>                                        #
+#                                                                                 #
+# NOTICES:                                                                        #
+#                                                                                 #
+# License: MIT                                                                    #
+#                                                                                 #
+###################################################################################
+
+
+
+def main()
+    puts("Hello, Ruby.")
+
+end # end of function main
